4baf0cca5faba36a90f6b48588f9a8c473f27b3f
this-is-emon <mofijul.islam.emon@gmail.com>
2024-01-11 11:26:17 +0600

    comment on InputText

--- 03counter/src/components/InputText/InputText.jsx
+++ 03counter/src/components/InputText/InputText.jsx
@@ -1,3 +1,7 @@
+// In this example, the text state variable holds a string. When you type,
+// handleChange reads the latest input value from the browser input DOM element,
+// and calls setText to update the state. This allows you to display the current
+// text below.
 // eslint-disable-next-line no-unused-vars
 import React, { useState } from "react";
 
