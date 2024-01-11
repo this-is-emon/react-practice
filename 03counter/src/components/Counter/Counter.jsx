22ad05c0a0987124088957bd7a21031a9fcceb51
this-is-emon <mofijul.islam.emon@gmail.com>
2024-01-11 11:06:59 +0600

    summary comment added

--- 03counter/src/components/Counter/Counter.jsx
+++ 03counter/src/components/Counter/Counter.jsx
@@ -1,3 +1,4 @@
+// In this example, the count state variable holds a number. Clicking the button increments it.
 // eslint-disable-next-line no-unused-vars
 import React, { useState } from "react";
 
